1905326f263d7051ae4d1c0bc9b56c5e63eb8bec
Mariana Hans <marianahansv@gmail.com>
2026-01-31 15:56:01 -0600

    linked game start, game page, game over

--- scripts/main_level.gd
+++ scripts/main_level.gd
@@ -6,7 +6,14 @@ var levels_passed = 0
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func game_over():
+	$GameOverLabel.visible = true
+	await get_tree().create_timer(0.5).timeout
+	$GameOverLabel.visible = false
+	get_tree().change_scene_to_file("res://game-over-screen.tscn")
+	
+	
