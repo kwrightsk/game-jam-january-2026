2ff1d8df86b91ef06d6ba237134390ad39079273
Katherine Nguyen <katherinenguyen0205@gmail.com>
2026-01-31 13:58:45 -0600

    organized all the files into script, scene and assets folder

--- scripts/paintbrush_cursor.gd
+++ scripts/paintbrush_cursor.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-var brush_up = load("res://paintbrush-cursor.png")
-var brush_down = load("res://paintbrush-cursor-down.png")
+var brush_up = load("res://assets/paintbrush-cursor.png")
+var brush_down = load("res://assets/paintbrush-cursor-down.png")
 
 func _ready() -> void:
 	Input.set_custom_mouse_cursor(brush_up)
