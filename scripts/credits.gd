bf7cfcb80f015ac31e54e2243f6ab7f1a9288599
Shruti Kaur <shrutikaren.k@gmail.com>
2026-02-01 15:47:25 -0600

    pushing the back sound to credits page

--- scripts/credits.gd
+++ scripts/credits.gd
@@ -1,4 +1,5 @@
 extends Node2D
+@onready var button_sound = $BackButton/AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,5 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_back_button_pressed() -> void:
+	button_sound.play()
+	await get_tree().create_timer(0.3).timeout
 	load("res://Scenes/title_screen.tscn")
 	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
