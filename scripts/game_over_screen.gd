7c985a228f5ef1a2d04fab1af3df182366e568ff
Shruti Kaur <shrutikaren.k@gmail.com>
2026-02-01 13:53:07 -0600

    when hover the main menu button works

--- scripts/game_over_screen.gd
+++ scripts/game_over_screen.gd
@@ -1,11 +1,15 @@
 extends Node2D
-
+@onready var hover_main_menu = $MainMenuButton/HoverMainMenu
+@onready var hover_leaderboard = $LeaderBoardButton/HoverLeaderBoard
+@onready var hover_play_again = $PlayAgainButton/HoverPlayAgain
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Globals.round = 1
-
-
+	hover_main_menu.visible = false
+	hover_leaderboard.visible = false
+	hover_play_again.visible = false
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -21,3 +25,9 @@ func _on_leader_board_button_pressed() -> void:
 func _on_main_menu_button_pressed() -> void:
 	print("main menu")
 	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
+
+func _on_main_menu_button_mouse_entered() -> void:
+	hover_main_menu.visible = true
+
+func _on_main_menu_button_mouse_exited() -> void:
+	hover_main_menu.visible = false
