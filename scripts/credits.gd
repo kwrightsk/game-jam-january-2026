90d038306714d9ea67ab4aa15e591d90e41daaf4
Shruti Kaur <shrutikaren.k@gmail.com>
2026-02-01 15:54:17 -0600

    changing the color of the credits

--- scripts/credits.gd
+++ scripts/credits.gd
@@ -1,10 +1,10 @@
 extends Node2D
 @onready var button_sound = $BackButton/AudioStreamPlayer
+@onready var hover_back_button = $BackButton/Panel2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	hover_back_button.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,3 +16,11 @@ func _on_back_button_pressed() -> void:
 	await get_tree().create_timer(0.3).timeout
 	load("res://Scenes/title_screen.tscn")
 	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
+
+
+func _on_back_button_mouse_entered() -> void:
+	hover_back_button.visible = true
+
+
+func _on_back_button_mouse_exited() -> void:
+	hover_back_button.visible = false
